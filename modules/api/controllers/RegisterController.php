33997ac993e6fb9b11c0347d6982024ddd6387f5
Anton Morev <shikto@mail.ru>
2019-08-30 01:14:10 +0300

    wip

--- modules/api/controllers/RegisterController.php
+++ modules/api/controllers/RegisterController.php
@@ -12,6 +12,9 @@ namespace Zvinger\Telegram\modules\api\controllers;
 use yii\filters\auth\HttpBearerAuth;
 use yii\web\BadRequestHttpException;
 use Zvinger\BaseClasses\api\controllers\BaseApiController;
+use Zvinger\Telegram\exceptions\connection\TelegramWrongConfirmCodeException;
+use Zvinger\Telegram\handlers\user_connection\models\UserInfoSetData;
+use Zvinger\Telegram\handlers\user_connection\UserConnectionInfoHandler;
 use Zvinger\Telegram\modules\api\models\register\initRegister\TelegramInitRegisterRequest;
 use Zvinger\Telegram\modules\api\models\register\saveCode\TelegramSaveCodeRequest;
 
@@ -32,9 +35,11 @@ class RegisterController extends BaseApiController
 
     public function actionCurrentData()
     {
+        $info = $this->prepareHandler()->getTelegramInfo();
+
         return [
-            'username' => '@amorev',
-            'telegramId' => 51817529,
+            'username' => 'User #'.$info->telegramId,
+            'telegramId' => $info->telegramId,
         ];
     }
 
@@ -42,22 +47,49 @@ class RegisterController extends BaseApiController
     {
         $request = TelegramInitRegisterRequest::createRequest();
         if (empty($request->telegramId)) {
-            throw new BadRequestHttpException("TelegramID не может быть пустым");
+            throw new BadRequestHttpException("TelegramID РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј");
         }
-        \Yii::$app->telegram->message($request->telegramId, 'Your code is 123123');
+        $this->prepareHandler()->setTelegramInfo(
+            \Yii::createObject(
+                [
+                    'class' => UserInfoSetData::class,
+                    'telegram_id' => $request->telegramId,
+                ]
+            )
+        );
     }
 
     public function actionSaveCode()
     {
         $request = TelegramSaveCodeRequest::createRequest();
         if (empty($request->telegramId)) {
-            throw new BadRequestHttpException("TelegramID не может быть пустым");
+            throw new BadRequestHttpException("TelegramID РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј");
         }
         if (empty($request->code)) {
-            throw new BadRequestHttpException("Code не может быть пустым");
+            throw new BadRequestHttpException("Code РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј");
         }
-        if ($request->code != 123123) {
-            throw new BadRequestHttpException("Код неверен");
+        try {
+            $this->prepareHandler()->confirmTelegramId(
+                \Yii::createObject(
+                    [
+                        'class' => UserInfoSetData::class,
+                        'telegram_id' => $request->telegramId,
+                        'confirmCode' => $request->code,
+                    ]
+                )
+            );
+        } catch (TelegramWrongConfirmCodeException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        } catch (\Exception $e) {
+            throw $e;
         }
+    }
+
+    /**
+     * @return UserConnectionInfoHandler
+     */
+    private function prepareHandler()
+    {
+        return \Yii::$app->telegram->getUserConnectionInfoHandler()->setUserId(\Yii::$app->user->id);
     }
 }
